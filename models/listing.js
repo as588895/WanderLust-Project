61791375aeee946404512b719ac0f537b13af848
Aman Singh <as588895@gmail.com>
2026-03-18 19:53:43 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema ({
+    // title: {
+    //     type: String,
+    //     required: true,
+    // },
+    title: String,
+    description: String,
+    // image: {
+    //     type: String,
+    //     default: "https://www.pexels.com/search/sunset%20tree/",
+    //     set: (v) => 
+    //         v === "" 
+    //           ? "https://www.pexels.com/search/sunset%20tree/" 
+    //             : v,
+    // },
+    image: {
+        filename: String,
+        url: String,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
