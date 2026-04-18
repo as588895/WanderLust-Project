c3c3cec165c2864bc1357909ca1f96a747a30a0f
Aman Singh <as588895@gmail.com>
2026-04-18 22:15:26 +0530

    some changes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -45,19 +47,38 @@ app.get("/listings", wrapAsync(async(req, res) => {
     res.render("listings/new.ejs");
    });
 
+   const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+        
+        if(error){
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+        }else {
+            next();
+        }
+   };
+
+   const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+        
+        if(error){
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+        }else {
+            next();
+        }
+   };
+
    //Show Route
    app.get("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
    }));
 
    //Create Route
-   app.post("/listings",
+   app.post("/listings", validateListing,
     wrapAsync(async(req, res, next) => {
-        if(!req.body.listing){
-            throw new ExpressError(400, "Send valid data for listing");
-        }
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
@@ -73,7 +94,7 @@ app.get("/listings", wrapAsync(async(req, res) => {
    }));
 
    //Update Route
-   app.put("/listings/:id", wrapAsync(async (req, res) => {
+   app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing });
     res.redirect(`/listings/${id}`);
@@ -85,7 +106,33 @@ app.get("/listings", wrapAsync(async(req, res) => {
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-   }));
+   })
+);
+
+//Reviews
+//Post  Review Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync( async(req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    await newReview.save();
+
+    listing.reviews.push(newReview._id);
+    await listing.save();
+
+     res.redirect("/listings/" + listing._id);
+}));
+
+//Delete Route
+app.delete("/listings/:id/reviews/:reviewId",
+    wrapAsync(async(req, res) => {
+        let { id, reviewId} = req.params;
+
+        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+        await Review.findByIdAndDelete(reviewId);
+
+        res.redirect(`/listings/${id}`);
+    })
+);
 
 // app.get("/testListing", async(req, res) => {
 //     let sampleListing = new Listing({
--- models/listing.js
+++ models/listing.js
@@ -1,28 +1,39 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema ({
-    // title: {
-    //     type: String,
-    //     required: true,
-    // },
     title: String,
     description: String,
     image: {
         type: String,
-        default: "https://www.pexels.com/search/sunset%20tree/",
+        default: "https://static.vecteezy.com/system/resources/thumbnails/054/880/166/small/thriving-tree-in-lush-green-environment-nature-conservation-and-protection-concept-free-photo.jpeg",
         set: (v) => 
             v === "" 
-              ? "https://www.pexels.com/search/sunset%20tree/" 
-                : v,
+              ? "https://static.vecteezy.com/system/resources/thumbnails/054/880/166/small/thriving-tree-in-lush-green-environment-nature-conservation-and-protection-concept-free-photo.jpeg"
+                : v
     },
-    // image: {
-    //     filename: String,
-    //     url: String,
-    // },
+    image: {
+        filename: String,
+        url: String,
+    },
+    
+
     price: Number,
     location: String,
     country: String,
+    reviews :[
+       {
+        type : Schema.Types.ObjectId,
+        ref : "Review",
+       },
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing) {
+        await Review.deleteMany({_id: { $in: listing.reviews }});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
